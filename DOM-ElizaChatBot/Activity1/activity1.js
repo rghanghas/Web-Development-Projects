68a0919e5fd0526e075b293b0a1d5c558fd9ac5b
Rahul Ghanghas <161168787+rghanghas@users.noreply.github.com>
2024-03-05 04:25:40 +0000

    DOM-ElizaChatBot

---
+++ DOM-ElizaChatBot/Activity1/activity1.js
@@ -0,0 +1,11 @@
+//Part1
+document.getElementsByClassName("footer__card__title tx-clr--slate")[1].innerHTML="Help Spread Love!";
+
+//Part2
+document.getElementsByTagName("li").length;
+
+//Part3
+document.getElementById("search_form_input").value;
+
+//Part4
+document.querySelector('.header__logo-wrap.js-header-logo').remove();
---
+++ DOM-ElizaChatBot/README.txt
@@ -0,0 +1,3 @@
+1.1 "Help Spread DuckDuckGo" message was at the bottom of the page rather than at the upper right.
+2. I put the dictionary in the js file, so that I don't have to reference it since we could only submit the html and js file.
+3. Similar to 2nd activity, I put the dictionary in the js file.
